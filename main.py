7a01c74140eaf9453237d24e692b7a8ccdb69964
KatrinSafi <167858422+KatrinSafi@users.noreply.github.com>
2024-10-19 21:41:40 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+my_dict = {"Ekaterina" : 1988, "Tatiana" : 1967, "Vas'ka" : 2023}
+print(my_dict)
+print(my_dict["Tatiana"])
+print(my_dict.get("Ramina"))
+my_dict.update({"Alexsandr": 2000,
+                "Mariya": 1995})
+print(my_dict)
+a = my_dict.pop("Vas'ka")
+print(my_dict.get)
+print(a)
+print(my_dict)
+
+my_set = {1,2,3,4,5,"milk",1,2,3,4,5,6,"milk","honey",True}
+print(my_set)
+print(my_set.add(17))
+print(my_set)
+print(my_set.discard("milk"))
+print(my_set)
